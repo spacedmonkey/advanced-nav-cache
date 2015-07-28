c58dc20e8940bf9ed978cfbbe2f9abd1e8f8b854
spacedmonkey <spacedmonkey2@gmail.com>
2015-07-28 23:10:05 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,66 @@
+Advanced Nav Cache
+===================
+
+Cache wp_nav_menu out in object cached.
+
+If you wish to follow the development of this plugin, view the code on the official plugin [website](http://www.jonathandavidharris.co.uk/ "website") or follow me on twitter [@thespacedmonkey](https://twitter.com/thespacedmonkey)
+
+
+## Installation
+
+This section describes how to install the plugin and get it working.
+
+
+### Using The WordPress Dashboard 
+
+1. Navigate to the 'Add New' in the plugins dashboard
+2. Search for 'advanced-nav-cache'
+3. Click 'Install Now'
+4. Activate the plugin on the Plugin dashboard
+
+### Uploading in WordPress Dashboard
+
+1. Navigate to the 'Add New' in the plugins dashboard
+2. Navigate to the 'Upload' area
+3. Select `advanced-nav-cache.zip` from your computer
+4. Click 'Install Now'
+5. Activate the plugin in the Plugin dashboard
+
+### Using FTP 
+1. Download `advanced-nav-cache.zip`
+2. Extract the `advanced-nav-cache` directory to your computer
+3. Upload the `advanced-nav-cache` directory to the `/wp-content/plugins/` directory
+4. Activate the plugin in the Plugin dashboard
+
+
+## GitHub Updater
+
+The Advanced Nav Cache includes native support for the [GitHub Updater](https://github.com/afragen/github-updater) which allows you to provide updates to your WordPress plugin from GitHub.
+
+## License
+
+The Advanced Nav Cache is licensed under the GPL v2 or later.
+
+> This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License, version 2, as
+published by the Free Software Foundation.
+
+> This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+> You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
+
+## Contributions
+
+Anyone is welcome to contribute to Advanced Nav Cache
+
+There are various ways you can contribute:
+
+* Raise an issue on GitHub.
+* Send us a Pull Request with your bug fixes and/or new features.
+* Provide feedback and suggestions on enhancements.
---
+++ advanced-nav-cache.php
@@ -0,0 +1,304 @@
+<?php
+/**
+ * Cache wp_nav_menu out in object cached
+ *
+ *
+ * @package   Advanced_Nav_Cache
+ * @author    Jonathan Harris <jon@spacedmonkey.co.uk>
+ * @license   GPL-2.0+
+ * @link      http://www.jonathandavidharris.co.uk/
+ * @copyright 2015 Spacedmonkey
+ *
+ * @wordpress-plugin
+ * Plugin Name:        Advanced Nav Cache
+ * Plugin URI:         https://www.github.com/spacedmonkey/advanced-nav-cache
+ * Description:        Cache wp_nav_menu out in object cached
+ * Version:            1.0.0
+ * Author:             Jonathan Harris
+ * Author URI:         http://www.jonathandavidharris.co.uk/
+ * License:            GPL-2.0+
+ * License URI:        http://www.gnu.org/licenses/gpl-2.0.txt
+ * Domain Path:        /languages
+ * GitHub Plugin URI:  https://www.github.com/spacedmonkey/advanced-nav-cache
+ */
+
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
+
+/**
+ * Class Advanced_Nav_Cache
+ */
+class Advanced_Nav_Cache {
+	/**
+	 * @var string
+	 */
+	var $CACHE_GROUP_PREFIX = 'advanced_nav_cache_';
+
+	// Flag for temp (within one page load) turning invalidations on and off
+	// @see dont_clear_advanced_nav_cache()
+	// @see do_clear_advanced_nav_cache()
+	// Used to prevent invalidation during new comment
+	/**
+	 * @var bool
+	 */
+	var $do_flush_cache = true;
+
+	// Flag for preventing multiple invalidations in a row: clean_post_cache() calls itself recursively for post children.
+	/**
+	 * @var bool
+	 */
+	var $need_to_flush_cache = true; // Currently disabled
+
+	/* Per cache-clear data */
+	/**
+	 * @var int
+	 */
+	var $cache_incr = 0; // Increments the cache group (advanced_nav_cache_0, advanced_nav_cache_1, ...)
+	/**
+	 * @var string
+	 */
+	var $cache_group = ''; // CACHE_GROUP_PREFIX . $cache_incr
+
+	// custom caching rules
+	/**
+	 * @var bool
+	 */
+	var $cache_on_single;
+	/**
+	 * @var bool
+	 */
+	var $cache_on_search;
+
+	/**
+	 *
+	 */
+	function __construct() {
+		// Specific to certain Memcached Object Cache plugins
+		if ( function_exists( 'wp_cache_add_group_prefix_map' ) ) {
+			wp_cache_add_group_prefix_map( $this->CACHE_GROUP_PREFIX, 'advanced_nav_cache' );
+		}
+
+		defined( 'ANC_ENABLE_ON_SINGLE' ) or define( 'ANC_ENABLE_ON_SINGLE', true );
+
+		defined( 'ANC_ENABLE_ON_SEARCH' ) or define( 'ANC_ENABLE_ON_SEARCH', true );
+
+		$this->cache_on_single = ANC_ENABLE_ON_SINGLE;
+		$this->cache_on_search = ANC_ENABLE_ON_SEARCH;
+
+
+		$this->setup_for_blog();
+
+		add_action( 'switch_blog', array( $this, 'setup_for_blog' ), 10, 2 );
+
+		add_action( 'clean_term_cache', array( $this, 'clear_advanced_nav_cache' ) );
+		add_action( 'clean_post_cache', array( $this, 'clear_advanced_nav_cache' ) );
+
+		// Don't clear Advanced Post Cache for a new comment - temp core hack
+		// http://core.trac.wordpress.org/ticket/15565
+		add_action( 'wp_updating_comment_count', array( $this, 'dont_clear_advanced_nav_cache' ) );
+		add_action( 'wp_update_comment_count', array( $this, 'do_clear_advanced_nav_cache' ) );
+
+		add_filter( 'pre_wp_nav_menu', array( $this, 'pre_wp_nav_menu' ), 9, 2 );
+		add_filter( 'wp_nav_menu', array( $this, 'wp_nav_menu' ), 99, 2 );
+
+	}
+
+	/**
+	 * @param bool $new_blog_id
+	 * @param bool $previous_blog_id
+	 */
+	function setup_for_blog( $new_blog_id = false, $previous_blog_id = false ) {
+		if ( $new_blog_id && $new_blog_id == $previous_blog_id ) {
+			return;
+		}
+
+		$this->cache_incr = wp_cache_get( 'advanced_nav_cache', 'cache_incrementors' ); // Get and construct current cache group name
+		if ( ! is_numeric( $this->cache_incr ) ) {
+			$now = time();
+			wp_cache_set( 'advanced_nav_cache', $now, 'cache_incrementors' );
+			$this->cache_incr = $now;
+		}
+		$this->cache_group = $this->CACHE_GROUP_PREFIX . $this->cache_incr;
+	}
+
+	/**
+	 * @param $output
+	 * @param $args
+	 *
+	 * @return string
+	 */
+	public function pre_wp_nav_menu( $output, $args ) {
+		if ( $this->isLoadFromCacheEnabled( $args ) ) {
+			$cached_value = wp_cache_get( $this->get_key( $args ), $this->cache_group );
+			if ( $cached_value !== false ) {
+				$output = $cached_value;
+			}
+		}
+
+		return $output;
+	}
+
+	/**
+	 * @param $output
+	 * @param $args
+	 *
+	 * @return string
+	 */
+	public function wp_nav_menu( $output, $args ) {
+		if ( $this->isLoadFromCacheEnabled( $args ) ) {
+			$cached_value = wp_cache_get( $this->get_key( $args ), $this->cache_group );
+			if ( $cached_value === false ) {
+				wp_cache_set( $this->get_key( $args ), $output, $this->cache_group );
+			}
+		}
+
+		return $output;
+	}
+
+	/**
+	 * @param $args
+	 *
+	 * @return string
+	 */
+	public function get_key( $args ) {
+
+		$object_id = $this->make_cache_key( $this->get_variabled() );
+		$flat_args = $this->make_cache_key( $args );
+		$cache_key = sprintf( '%s_%s', $object_id, $flat_args );
+
+		return $cache_key;
+	}
+
+	/**
+	 * Per page cache salt. If 404 page, just return the same menu as other 404 pages.
+	 * Also, on secondary pages, return the same menu. Better for performance.
+	 *
+	 * @return string
+	 */
+	public function get_variabled() {
+		global $wp_query;
+
+		if ( $wp_query->is_404() ) {
+			$page_variable = array( 'error_404' );
+		} else {
+			$page_variable = $wp_query->query_vars;
+
+		}
+		$page_variable['paged'] = 0;
+		asort( $page_variable );
+
+		return $page_variable;
+	}
+
+	/**
+	 * @param $key
+	 *
+	 * @return string
+	 */
+	public function make_cache_key( $key ) {
+		return md5( serialize( $key ) );
+	}
+
+	/**
+	 *
+	 */
+	public function clear_advanced_nav_cache() {
+		$this->flush_cache();
+	}
+
+	/**
+	 *
+	 */
+	public function do_clear_advanced_nav_cache() {
+		$this->do_flush_cache = true;
+	}
+
+	/**
+	 *
+	 */
+	public function dont_clear_advanced_nav_cache() {
+		$this->do_flush_cache = false;
+	}
+
+	/* Advanced Post Cache API */
+
+	/**
+	 * Flushes the cache by incrementing the cache group
+	 */
+	public function flush_cache() {
+		// Cache flushes have been disabled
+		if ( ! $this->do_flush_cache ) {
+			return;
+		}
+
+		// Bail on post preview
+		if ( is_admin() && isset( $_POST['wp-preview'] ) && 'dopreview' == $_POST['wp-preview'] ) {
+			return;
+		}
+
+		// Bail on autosave
+		if ( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE ) {
+			return;
+		}
+
+		// We already flushed once this page load, and have not put anything into the cache since.
+		// OTHER processes may have put something into the cache!  In theory, this could cause stale caches.
+		// We do this since clean_post_cache() (which fires the action this method attaches to) is called RECURSIVELY for all descendants.
+//		if ( !$this->need_to_flush_cache )
+//			return;
+
+		$this->cache_incr = wp_cache_incr( 'advanced_nav_cache', 1, 'cache_incrementors' );
+		if ( 10 < strlen( $this->cache_incr ) ) {
+			wp_cache_set( 'advanced_nav_cache', 0, 'cache_incrementors' );
+			$this->cache_incr = 0;
+		}
+		$this->cache_group         = $this->CACHE_GROUP_PREFIX . $this->cache_incr;
+		$this->need_to_flush_cache = false;
+	}
+
+	/**
+	 *
+	 * @return boolean
+	 */
+	public function isLoadFromCacheEnabled( $args = array() ) {
+		$enabled = true;
+
+		if ( is_admin() ) {
+			$enabled = false;
+		}
+		if ( is_feed() ) {
+			$enabled = false;
+		}
+		if ( is_preview() ) {
+			$enabled = false;
+		}
+		if ( function_exists( 'is_customize_preview' ) && is_customize_preview() ) {
+			$enabled = false;
+		}
+		if ( defined( 'DOING_AJAX' ) && DOING_AJAX ) {
+			$enabled = false;
+		}
+		if ( defined( 'DOING_CRON' ) && DOING_CRON ) {
+			$enabled = false;
+		}
+		if ( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE ) {
+			$enabled = false;
+		}
+
+		if ( is_search() ) {
+			$enabled = $this->cache_on_search;
+		}
+
+		if ( is_singular() && ! is_front_page() ) {
+			$enabled = $this->cache_on_single;
+		}
+
+		return apply_filters( 'filter_name', $enabled, $args );
+	}
+
+}
+
+global $advanced_nav_cache_object;
+$advanced_nav_cache_object = new Advanced_Nav_Cache();
